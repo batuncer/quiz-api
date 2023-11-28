084860e8212d3752a3f79d9e3c9dfe1d27847bf9
baki <tuncerbaki@gmail.com>
2023-11-28 12:02:50 +0000

    players names uppercase

--- src/services/quiz.js
+++ src/services/quiz.js
@@ -33,11 +33,16 @@ async function saveQuizAsync(quiz) {
 async function getAllQuizzesAsync() {
     const quizzes = await db.getAllQuizzes();
     const list = quizzes.map(quiz => {
+
         return {
-            id: quiz.id,
-            name: quiz.name
+        
+        id: quiz.id,
+        
+        name: quiz.name.toUpperCase()
+        
         };
-    });
+        
+        })
     return list;
 }
 
